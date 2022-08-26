bdd9c803079260570d6cd34387574a7605c8f552
IceTour <nonejmv@gmail.com>
2022-08-26 21:09:11 +0300

    #

--- system/core/Router.php
+++ system/core/Router.php
@@ -7,25 +7,9 @@ use RegexIterator;
 class Router
 {
     public string $url;
-    public $method;
     public array $params;
 
     public function get(): self
-    {
-        return $this;
-    }
-    
-    public function group()
-    {
-        return $this;
-    }
-
-    public function auth()
-    {
-        return $this;
-    }
-
-    public function prefix()
     {
         return $this;
     }
